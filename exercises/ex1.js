e1d93f22b62d7e4a7094a5abdf642c85806be2ec
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-07-27 21:19:46 -0400

    create a script

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,1 +1,15 @@
-// My first Node.js program
+#! /usr/bin/env node
+
+'use strict';
+
+// printHelp();
+
+// **********************
+
+function printHelp() {
+  console.log('ex1 usage:');
+  console.log('  ex1.js --help');
+  console.log('');
+  console.log('--help           print this help');
+  console.log('');
+}

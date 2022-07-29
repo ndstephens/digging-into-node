b751a14b20526b148f044b6488d832ff038b630a
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-07-29 14:28:36 -0400

    server

--- exercises/ex5.js
+++ exercises/ex5.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
-var util = require("util");
-var path = require("path");
-// var http = require("http");
+var util = require('util');
+var path = require('path');
+var http = require('http');
 
-var sqlite3 = require("sqlite3");
-// var staticAlias = require("node-static-alias");
-
+var sqlite3 = require('sqlite3');
+var staticAlias = require('node-static-alias');
 
 // ************************************
 
-const DB_PATH = path.join(__dirname,"my.db");
-const WEB_PATH = path.join(__dirname,"web");
+const DB_PATH = path.join(__dirname, 'my.db');
+const WEB_PATH = path.join(__dirname, 'web');
 const HTTP_PORT = 8039;
 
 var delay = util.promisify(setTimeout);
@@ -22,35 +21,73 @@ var delay = util.promisify(setTimeout);
 //   (comment out if sqlite3 not working for you)
 var myDB = new sqlite3.Database(DB_PATH);
 var SQL3 = {
-	run(...args) {
-		return new Promise(function c(resolve,reject){
-			myDB.run(...args,function onResult(err){
-				if (err) reject(err);
-				else resolve(this);
-			});
-		});
-	},
-	get: util.promisify(myDB.get.bind(myDB)),
-	all: util.promisify(myDB.all.bind(myDB)),
-	exec: util.promisify(myDB.exec.bind(myDB)),
+  run(...args) {
+    return new Promise(function c(resolve, reject) {
+      myDB.run(...args, function onResult(err) {
+        if (err) reject(err);
+        else resolve(this);
+      });
+    });
+  },
+  get: util.promisify(myDB.get.bind(myDB)),
+  all: util.promisify(myDB.all.bind(myDB)),
+  exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
-// var fileServer = new staticAlias.Server(WEB_PATH,{
-// 	cache: 100,
-// 	serverInfo: "Node Workshop: ex5",
-// 	alias: [
-// 	],
-// });
+// Static file server
+var fileServer = new staticAlias.Server(WEB_PATH, {
+  cache: 100,
+  serverInfo: 'Node Workshop: ex5',
+  alias: [
+    {
+      match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
+      serve: 'index.html',
+      force: true,
+    },
+    {
+      match: /^\/js\/.+$/,
+      serve: '<% absPath %>',
+      force: true,
+    },
+    {
+      match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
+      serve: function onMatch(params) {
+        return `${params.basename}.html`;
+      },
+    },
+    {
+      match: /[^]/,
+      serve: '404.html',
+    },
+  ],
+});
 
-// var httpserv = http.createServer(handleRequest);
+var httpserv = http.createServer(handleRequest);
 
 main();
-
 
 // ************************************
 
 function main() {
-	// console.log(`Listening on http://localhost:${HTTP_PORT}...`);
+  httpserv.listen(HTTP_PORT);
+  console.log(`Listening on http://localhost:${HTTP_PORT}`);
+}
+
+async function handleRequest(req, res) {
+  if (req.url === '/get-records') {
+    // Create a fake delay of 1 second
+    await delay(1000);
+
+    const records = await getAllRecords();
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    });
+    res.end(JSON.stringify(records));
+  } else {
+    fileServer.serve(req, res);
+  }
 }
 
 // *************************
@@ -58,8 +95,8 @@ function main() {
 //   comment this version out
 // *************************
 async function getAllRecords() {
-	var result = await SQL3.all(
-		`
+  var result = await SQL3.all(
+    `
 		SELECT
 			Something.data AS "something",
 			Other.data AS "other"
@@ -69,9 +106,9 @@ async function getAllRecords() {
 		ORDER BY
 			Other.id DESC, Something.data
 		`
-	);
+  );
 
-	return result;
+  return result;
 }
 
 // *************************
--- exercises/web/js/ex5-web.js
+++ exercises/web/js/ex5-web.js
@@ -1,43 +1,50 @@
-(function Ex5(){
-	var btn;
-	var recordsList;
+(function Ex5() {
+  var btn;
+  var recordsList;
 
-	document.addEventListener("DOMContentLoaded",init,false);
+  document.addEventListener('DOMContentLoaded', init, false);
 
-	// **************
+  // **************
 
-	function init() {
-		btn = document.querySelectorAll("[rel*=js-btn]")[0];
-		recordsList = document.querySelectorAll("[rel*=js-records-list]")[0];
+  function init() {
+    btn = document.querySelectorAll('[rel*=js-btn]')[0];
+    recordsList = document.querySelectorAll('[rel*=js-records-list]')[0];
 
-		btn.addEventListener("click",getRecords,false);
-	}
+    btn.addEventListener('click', getRecords, false);
+  }
 
-	function renderRecords(records) {
-		var transforms = {
-			"row": {
-				"<>": "tr",
-				html: "<td>${something}</td><td>${other}</td>",
-			},
-			"table": {
-				"<>": "table",
-				border: "1",
-				cellPadding: "10",
-				html: function table(){
-					return `<tr><td>Something</td><td>Other</td></tr>
-						${json2html.transform(records,transforms.row)}
+  function renderRecords(records) {
+    var transforms = {
+      row: {
+        '<>': 'tr',
+        html: '<td>${something}</td><td>${other}</td>',
+      },
+      table: {
+        '<>': 'table',
+        border: '1',
+        cellPadding: '10',
+        html: function table() {
+          return `<tr><td>Something</td><td>Other</td></tr>
+						${json2html.transform(records, transforms.row)}
 					`;
-				},
-			},
-		};
+        },
+      },
+    };
 
-		recordsList.innerHTML = json2html.transform({},transforms.table);
-	}
+    recordsList.innerHTML = json2html.transform({}, transforms.table);
+  }
 
-	async function getRecords() {
-		recordsList.innerHTML = "...";
+  async function getRecords() {
+    // While waiting for response:
+    recordsList.innerHTML = '...';
 
-		// TODO
-	}
-
+    // Fetch
+    const res = await fetch('/get-records');
+    if (res?.ok) {
+      const records = await res.json();
+      if (records?.length) {
+        renderRecords(records);
+      }
+    }
+  }
 })();

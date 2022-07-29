6b8ad67a8f522fc26bcecda317ca91970b56a1fb
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-07-29 16:25:34 -0400

    server with express

--- exercises/ex6.js
+++ exercises/ex6.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
-var util = require("util");
-var path = require("path");
-var http = require("http");
+var util = require('util');
+var path = require('path');
+var http = require('http');
 
-// var express = require("express");
-var sqlite3 = require("sqlite3");
+var express = require('express');
+var sqlite3 = require('sqlite3');
 
+const app = express();
 
 // ************************************
 
-const DB_PATH = path.join(__dirname,"my.db");
-const WEB_PATH = path.join(__dirname,"web");
+const DB_PATH = path.join(__dirname, 'my.db');
+const WEB_PATH = path.join(__dirname, 'web');
 const HTTP_PORT = 8039;
 
 var delay = util.promisify(setTimeout);
@@ -22,55 +23,67 @@ var delay = util.promisify(setTimeout);
 //   (comment out if sqlite3 not working for you)
 var myDB = new sqlite3.Database(DB_PATH);
 var SQL3 = {
-	run(...args) {
-		return new Promise(function c(resolve,reject){
-			myDB.run(...args,function onResult(err){
-				if (err) reject(err);
-				else resolve(this);
-			});
-		});
-	},
-	get: util.promisify(myDB.get.bind(myDB)),
-	all: util.promisify(myDB.all.bind(myDB)),
-	exec: util.promisify(myDB.exec.bind(myDB)),
+  run(...args) {
+    return new Promise(function c(resolve, reject) {
+      myDB.run(...args, function onResult(err) {
+        if (err) reject(err);
+        else resolve(this);
+      });
+    });
+  },
+  get: util.promisify(myDB.get.bind(myDB)),
+  all: util.promisify(myDB.all.bind(myDB)),
+  exec: util.promisify(myDB.exec.bind(myDB)),
 };
 
 var httpserv = http.createServer(app);
 
-
 main();
-
 
 // ************************************
 
 function main() {
-	// TODO: define routes
-	//
-	// Hints:
-	//
-	// {
-	// 	match: /^\/(?:index\/?)?(?:[?#].*$)?$/,
-	// 	serve: "index.html",
-	// 	force: true,
-	// },
-	// {
-	// 	match: /^\/js\/.+$/,
-	// 	serve: "<% absPath %>",
-	// 	force: true,
-	// },
-	// {
-	// 	match: /^\/(?:[\w\d]+)(?:[\/?#].*$)?$/,
-	// 	serve: function onMatch(params) {
-	// 		return `${params.basename}.html`;
-	// 	},
-	// },
-	// {
-	// 	match: /[^]/,
-	// 	serve: "404.html",
-	// },
+  defineRoutes();
+  httpserv.listen(HTTP_PORT);
+  console.log(`Listening on http://localhost:${HTTP_PORT}...`);
+}
 
-	httpserv.listen(HTTP_PORT);
-	console.log(`Listening on http://localhost:${HTTP_PORT}...`);
+function defineRoutes() {
+  app.get('/get-records', async (req, res) => {
+    await delay(1000);
+    const records = (await getAllRecords()) || [];
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    });
+    res.end(JSON.stringify(records));
+  });
+
+  // Rewire url string
+  app.use(function (req, res, next) {
+    if (/^\/(?:index\/?)?(?:[?#].*$)?$/.test(req.url)) {
+      req.url = '/index.html';
+    } else if (/^\/js\/.+$/.test(req.url)) {
+      next();
+      return;
+    } else if (/^\/(?:[\w\d]+)(?:[\/?#].*$)?$/.test(req.url)) {
+      let [, basename] = req.url.match(/^\/([\w\d]+)(?:[\/?#].*$)?$/);
+      req.url = `${basename}.html`;
+    } else {
+      req.url = '/404.html';
+    }
+
+    next();
+  });
+
+  app.use(
+    express.static(WEB_PATH, {
+      maxAge: 100,
+      setHeaders: function setHeaders(res) {
+        res.setHeader('Server', 'Node Workshop: ex6');
+      },
+    })
+  );
 }
 
 // *************************
@@ -78,8 +91,8 @@ function main() {
 //   comment this version out
 // *************************
 async function getAllRecords() {
-	var result = await SQL3.all(
-		`
+  var result = await SQL3.all(
+    `
 		SELECT
 			Something.data AS "something",
 			Other.data AS "other"
@@ -89,9 +102,9 @@ async function getAllRecords() {
 		ORDER BY
 			Other.id DESC, Something.data
 		`
-	);
+  );
 
-	return result;
+  return result;
 }
 
 // *************************

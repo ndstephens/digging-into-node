d0019f42620b42019fc25f12c3ba2ebfe9eff677
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-07-29 17:35:30 -0400

    child processes, load test the server

--- exercises/ex7-child.js
+++ exercises/ex7-child.js
@@ -1,18 +1,26 @@
-"use strict";
+'use strict';
 
-// var fetch = require("node-fetch");
-
+var fetch = require('node-fetch');
 
 // ************************************
 
 const HTTP_PORT = 8039;
 
-
 main().catch(() => 1);
-
 
 // ************************************
 
 async function main() {
-	// TODO
+  try {
+    const res = await fetch('http://localhost:8039/get-records');
+    if (res?.ok) {
+      const records = await res.json();
+      if (records?.length) {
+        process.exitCode = 0;
+        return;
+      }
+    }
+  } catch (err) {}
+
+  process.exitCode = 1;
 }
